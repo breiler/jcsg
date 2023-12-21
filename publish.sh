5efbfede8f878ae543aaa67c14bd18f04f0ef564
Kevin Harrington <mad.hephaestus@gmail.com>
2023-12-21 11:57:25 -0500

    force the tag into the build so the jar knows what version it is

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ ls -al $HOME/.gnupg/
 
 ./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
 # copy the jar up for release
-
+echo "app.version=$VERSION_SEMVER" >./src/main/resources/com/neuronrobotics/javacad/build.properties
 # close and release Sonatype
 ./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
 
