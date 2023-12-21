9c69c77e45a190921f744a816c5e11669771fbb9
Kevin Harrington <mad.hephaestus@gmail.com>
2023-12-21 16:24:41 -0500

    set the semver before publishing

--- publish.sh
+++ publish.sh
@@ -5,9 +5,11 @@ gpg1 --list-secret-keys --keyid-format LONG
 
 ls -al $HOME/.gnupg/
 
-./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
-# copy the jar up for release
 echo "app.version=$VERSION_SEMVER" >./src/main/resources/com/neuronrobotics/javacad/build.properties
+
+
+./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
+
 # close and release Sonatype
 ./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
 
