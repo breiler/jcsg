17812cf311de793b41d395da0c55e55032a56c3b
Kevin Harrington <mad.hephaestus@gmail.com>
2023-12-21 11:34:04 -0500

    updating the the new publish in CI

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat <(echo -e $OSSRH_GPG_SECRET_KEY) | gpg1 --batch --import
+gpg1 --list-secret-keys --keyid-format LONG
+
+ls -al $HOME/.gnupg/
+
+./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
+# copy the jar up for release
+
+# close and release Sonatype
+./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
+
