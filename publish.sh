0410ac88098c2614716e34a6b080e8fbbacd349c
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-25 09:35:06 -0400

    Allow a longer timmeout for publishing and validation

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 cat <(echo -e $OSSRH_GPG_SECRET_KEY) | gpg1 --batch --import
 gpg1 --list-secret-keys --keyid-format LONG
 
@@ -8,7 +8,7 @@ ls -al $HOME/.gnupg/
 echo "app.version=$VERSION_SEMVER" >./src/main/resources/com/neuronrobotics/javacad/build.properties
 
 
-./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID
+./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID -PstagingProgressTimeoutMinutes=15
 
 # close and release Sonatype
 ./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
