4d6a33d0142e7016745cf2a1c1b5d1954565b897
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-25 10:42:38 -0400

    re-add the publish

--- publish.sh
+++ publish.sh
@@ -8,7 +8,7 @@ echo "app.version=$VERSION_SEMVER" >./src/main/resources/com/neuronrobotics/java
 
 set -e
 
-#./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID -PstagingProgressTimeoutMinutes=15
+./gradlew publish -Psigning.secretKeyRingFile=$HOME/.gnupg/secring.gpg -Psigning.password=$OSSRH_GPG_SECRET_KEY_PASSWORD -Psigning.keyId=$OSSRH_GPG_SECRET_KEY_ID -PstagingProgressTimeoutMinutes=15
 
 # close and release Sonatype
 #./gradlew closeAndReleaseRepository -PnexusUsername=$MAVEN_USERNAME -PnexusPassword=$MAVEN_PASSWORD
