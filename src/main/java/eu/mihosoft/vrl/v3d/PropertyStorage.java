e8bc0bd36b6d079012a6b84c585dd81478755a26
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-30 11:11:52 -0400

    remove junk import

--- src/main/java/eu/mihosoft/vrl/v3d/PropertyStorage.java
+++ src/main/java/eu/mihosoft/vrl/v3d/PropertyStorage.java
@@ -39,8 +39,6 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
-
 import javafx.scene.paint.Color;
 
 // TODO: Auto-generated Javadoc
